44d2143d373f566e5d44f748bbe56af906cd9ddf
Brandon <defoe.brand@gmail.com>
2020-07-22 13:44:26 +0300

    Create bubble_sort_by method

--- bubble_sort_by.rb
+++ bubble_sort_by.rb
@@ -1,3 +1,14 @@
-def bubble_sort_by (var)
-    yeild 
-end 
+def sift_through(arr)
+  arr.each_with_index do |_num, ind|
+    break if arr[ind + 1].nil?
+    next unless arr[ind].length > arr[ind + 1].length
+
+    arr[ind], arr[ind + 1] = arr[ind + 1], arr[ind]
+  end
+end
+
+def bubble_sort_by
+  arr = yield
+  arr.length.times { sift_through(arr) }
+  p arr
+end
